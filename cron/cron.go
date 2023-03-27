c93c53750e145da81e5100dcff3f8c9533d7d494
lzy-a <1160440131@qq.com>
2023-03-27 16:34:10 +0800

    cron

---
+++ cron/cron.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"gin/models"
+	"log"
+
+	"github.com/robfig/cron"
+)
+
+func main() {
+	log.Println("Starting...")
+
+	c := cron.New()
+	c.AddFunc("* * * * * *", func() {
+		log.Println("Run models.CleanAllTag...")
+		models.CleanAllTag()
+	})
+	c.AddFunc("* * * * * *", func() {
+		log.Println("Run models.CleanAllArticle...")
+		models.CleanAllArticle()
+	})
+
+	c.Start()
+
+	select {}
+}
